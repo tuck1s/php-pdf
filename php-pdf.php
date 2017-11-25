34b2d3e55d04efb9573ff74298b60241776fdf6c
tuck1s <steve.tuck@messagesystems.com>
2017-11-25 11:16:46 +0000

    Calling pdftk

--- php-pdf.php
+++ php-pdf.php
@@ -1,3 +1,29 @@
 <?php
+use mikehaertl\pdftk\Pdf;
+
 echo "PHP version " . phpversion();
+exec("pdftk --version", $out);
+print_r($out);
+
+/*
+// Fill form with data array
+$pdf = new Pdf('/Users/stuck/PhpstormProjects/php-pdf/myform.pdf');
+$pdf->fillForm([
+    'name'=>'ÄÜÖ äüö мирано čárka',
+    'nested.name' => 'valX',
+])
+    ->needAppearances()
+    ->saveAs('filled.pdf');
+
+// Fill form from FDF
+$pdf = new Pdf('form.pdf');
+$pdf->fillForm('data.xfdf')
+    ->saveAs('filled.pdf');
+
+// Check for errors
+if (!$pdf->saveAs('my.pdf')) {
+    $error = $pdf->getError();
+}
+
+*/
 ?>
