b3e754183e06a57dbd96fe8f46947a7a8ae26b30
tuck1s <steve.tuck@messagesystems.com>
2017-11-25 14:18:25 +0000

    Workaround for Heroku tool in path

--- php-pdf.php
+++ php-pdf.php
@@ -1,23 +1,27 @@
 <?php
-//use mikehaertl\pdftk\Pdf;
 
+echo '<pre>';
 echo "PHP version " . phpversion();
 
+$os = php_uname();
+
+// Check if running on Linux, e.g. Heroku
+if (strpos($os, 'Ubuntu') !== false) {
+    $wk = "/app/bin/wkhtmltopdf";
+}
+    else {
+        $wk = "wkhtmltopdf";                            // Mac appropriate
+}
+
 exec("pdftk --version 2>&1", $out);
-echo '<pre>';
 print_r($out); $out = NULL;
-echo  '</pre>';
 
-exec("wkhtmltopdf --version 2>&1", $out);
-echo '<pre>';
+exec($wk." --version 2>&1", $out);
 print_r($out); $out = NULL;
-echo  '</pre>';
 
-exec("wkhtmltopdf --page-size letter --dpi 300 --zoom 4 sample.html sample.pdf 2>&1", $out);
+exec($wk." --page-size letter --dpi 300 --zoom 4 sample.html sample.pdf 2>&1", $out);
 
-echo '<pre>';
 print_r($out); $out = NULL;
-echo  '</pre>';
 
 //phpinfo()
 ?>
