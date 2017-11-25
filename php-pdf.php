85cfd83ab294ff305c83f39d1546e5221a81f83f
tuck1s <steve.tuck@messagesystems.com>
2017-11-25 14:01:54 +0000

    trying cook html to pdf

--- php-pdf.php
+++ php-pdf.php
@@ -3,38 +3,21 @@
 
 echo "PHP version " . phpversion();
 
-exec("pdftk --version", $out);
+exec("pdftk --version 2>&1", $out);
 echo '<pre>';
-print_r($out);
+print_r($out); $out = NULL;
 echo  '</pre>';
 
-exec("wkhtmltopdf --version 2>&1", $out2);
+exec("wkhtmltopdf --version 2>&1", $out);
 echo '<pre>';
-print_r($out2);
+print_r($out); $out = NULL;
 echo  '</pre>';
 
-exec("wkhtmltopdf --page-size letter --dpi 300 --zoom 4 sample.html sample.pdf 2>&1");
-exec("open sample.pdf")
+exec("wkhtmltopdf --page-size letter --dpi 300 --zoom 4 sample.html sample.pdf 2>&1", $out);
 
-/*
-// Fill form with data array
-$pdf = new Pdf('/Users/stuck/PhpstormProjects/php-pdf/myform.pdf');
-$pdf->fillForm([
-    'name'=>'ÄÜÖ äüö мирано čárka',
-    'nested.name' => 'valX',
-])
-    ->needAppearances()
-    ->saveAs('filled.pdf');
+echo '<pre>';
+print_r($out); $out = NULL;
+echo  '</pre>';
 
-// Fill form from FDF
-$pdf = new Pdf('form.pdf');
-$pdf->fillForm('data.xfdf')
-    ->saveAs('filled.pdf');
-
-// Check for errors
-if (!$pdf->saveAs('my.pdf')) {
-    $error = $pdf->getError();
-}
-
-*/
+//phpinfo()
 ?>
